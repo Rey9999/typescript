a96c69208629732dee44d645bcbb39759b0e39aa
Rey9999 <103054282+Rey9999@users.noreply.github.com>
2022-10-11 11:29:49 +0200

    java

---
+++ address.java
@@ -0,0 +1,29 @@
+// export class Address {
+//     number: string;
+//     street: string;
+//     city: string;
+//     country: string;
+// }
+
+public class Address {
+    private Number number;
+    private String street;
+    private String city;
+    private String country
+
+    public Address(Number number, String street, String city, String country ) {
+        this.number = number;
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+
+   
+   public void getAdress(Number number, String street, String city, String country ) = { return {this.number = number;}  
+   {this.street = street;} 
+    {this.city = city;}
+   {this.country = country;} }
+
+
+}
+

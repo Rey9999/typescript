51db3343a316ef8e23c56ec6f7afb08a52f8494f
Rey9999 <103054282+Rey9999@users.noreply.github.com>
2022-10-11 11:38:25 +0200

    javaV2

--- address.java
+++ address.java
@@ -19,11 +19,21 @@ public class Address {
     }
 
    
-   public void getAdress(Number number, String street, String city, String country ) = { return {this.number = number;}  
-   {this.street = street;} 
-    {this.city = city;}
-   {this.country = country;} }
+    public String getNumber() {return number;}
+    public void setNumber(Number number) {this.number = number;}
+    
+    public String getStreet() {return street;}
+    public void setStreet(String street) {this.street = street;}
+    
+    public String getCity() {return city;}
+    public void setCity(String city) {this.city = city;}
+    
+    public String getCountry() {return country;}
+    public void setCountry(String country) {this.country = country;}
+    
+    
+    }
 
 
-}
+
 
--- photo.java
+++ photo.java
@@ -9,7 +9,7 @@ public class Photo {
     private String url;
     private String filename;
     private Number width;
-    private Number height 
+    private Number height ;
 
 
     public Photo(String url, String filename, Number width, Number height ) {
@@ -18,6 +18,19 @@ public class Photo {
         this.width = width;
         this.height = height;
     }
+
+    
+  public String getUrl() {return url;}
+  public void setUrl(String url) {this.url = url;}
+  
+  public String getFilename() {return filename;}
+  public void setFilename(String filename) {this.filename = filename;}
+  
+  public Float getWidth() {return width;}
+  public void setWidth(Number width) {this.width = width;}
+  
+  public Float getHeight() {return height;}
+  public void setHeight(Number height) {this.height = height;}
 }
 
 

88760e97dd8adb2e4048c69fbb7b4697d159eea5
Rey9999 <103054282+Rey9999@users.noreply.github.com>
2022-10-11 09:58:28 +0200

    person

--- person.ts
+++ person.ts
@@ -1,28 +1,25 @@
 class Person {
 
-name: string;
-age: number;
+name : string;
+   age : number;
 
-   constructor (name: string, age: number ){
+   constructor (name: string, age: number {
     this.name = name,
     this.age = age
-   }
+   })
+}
 
 tellMyName() : void {
     console.log(`I am ${this.name}`)
 }
 
 tellMyAge() : void {
+
     console.log(`I am ${this.age} years old`)
-}
-
-
 }
 
 const person1 = new Person ('John', 40)
 const person2 = new Person ('Mary', 35)
 
-person1.tellMyAge()
-person2.tellMyName()
-
-
+tellMyname(person1);
+tellMyAge(person1);
